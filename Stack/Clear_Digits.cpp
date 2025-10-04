70eede1a8bb94f5f1259eb1bda6bb312d3c1ef6f
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2025-10-04 20:34:00 +0530

    Create Clear_Digits.cpp

---
+++ Stack/Clear_Digits.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        int n=s.length();
+        string ans="";
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(!st.empty()){
+                if(s[i]>47 &&s[i]<58){
+                    char temp=st.top();
+                    if(temp>96&&temp<123){
+                        st.pop();
+                    } 
+                    else st.push(s[i]); 
+                }
+                else st.push(s[i]);
+            }
+            else st.push(s[i]);
+        }
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+         reverse(ans.begin(),ans.end());
+         return ans;
+    }
+};
