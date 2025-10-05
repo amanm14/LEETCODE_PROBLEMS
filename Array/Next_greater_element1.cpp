7f46b298d15ae56d59725ff022bf6e46e6a419cc
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2025-10-05 19:05:22 +0530

    Add files via upload

---
+++ Array/Next_greater_element1.cpp
@@ -0,0 +1,24 @@
+// TC-> O(n) SC->O(1) Its stack qus i solved it without it 
+
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                bool flag=0;
+                if(nums1[i]==nums2[j]){
+                    
+                    while(flag==0&&j<nums2.size()){
+                        if(nums1[i]<nums2[j]){
+                            nums1[i]=nums2[j];
+                            flag=1;
+                        }
+                        j++;
+                    }
+                    if(flag==0) nums1[i]=-1;
+                }
+            }
+        }
+        return nums1;
+    }
+};
