b8fd60dbecdc22f7550806b6b880f30844d2772d
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2025-10-20 23:53:24 +0530

    Create 350_Intersection_of_Two_Arrays_II.cpp

---
+++ Map/Easy/350_Intersection_of_Two_Arrays_II.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+// TC-> O(n2) SC-> O(n)
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int>ans;
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                if(nums1[i]==nums2[j]&&nums2[j]!=-1){
+                    ans.push_back(nums1[i]);
+                    nums2[j]=-1;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
