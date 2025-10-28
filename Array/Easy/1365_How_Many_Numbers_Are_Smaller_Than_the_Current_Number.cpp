06ccedc1ad3d4ee299089a2da02b518a727126cf
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2025-10-28 22:39:27 +0530

    Create 1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.cpp

---
+++ Array/Easy/1365_How_Many_Numbers_Are_Smaller_Than_the_Current_Number.cpp
@@ -0,0 +1,17 @@
+class Solution {// TC O(N2) SC O(N) can be optimised using map
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int>ans;
+        
+        for(int i=0;i<nums.size();i++){
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[i]>nums[j]){
+                    count++;
+                }
+            }
+            ans.push_back(count);
+        }
+        return ans;
+    }
+};
