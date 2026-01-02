53fbab47dac9dc36e4e606e0a7799ee6307fdb26
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-01-02 23:31:55 +0530

    Update 921_Minimum_Add_to_Make_Parentheses_Valid.cpp

--- Stack/Medium/921_Minimum_Add_to_Make_Parentheses_Valid.cpp
+++ Stack/Medium/921_Minimum_Add_to_Make_Parentheses_Valid.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char>st; //O(N) n is length of string is SC and TC
+        stack<char>st; //O(N) n is length of string is SC and TC we can use open and close variable to solve it in O(1) sc 
         for(int i=0;i<s.length();i++)
         {
             if(!st.empty()&&s[i]==')'&&st.top()=='(') st.pop();
