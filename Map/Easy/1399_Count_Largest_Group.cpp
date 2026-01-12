937de7f3f06eafcf6bf94d917b45fc1c630e0dc8
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-01-13 02:00:23 +0530

    Create 1399_Count_Largest_Group.cpp

---
+++ Map/Easy/1399_Count_Largest_Group.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+// Time Complexity: O(n log n)
+// Space Complexity: O(log n) as we have only 9 didgits klogn becomes logn
+    int countLargestGroup(int n) {
+        unordered_map<int, int> digitCountFreq;
+        int maxDigitCount = 0, maxDigitFreq = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int digitSum = 0, key = i;
+            while (key > 0) {
+                digitSum += key % 10;
+                key /= 10;
+            }
+            digitCountFreq[digitSum]++;
+            maxDigitCount = max(maxDigitCount, digitCountFreq[digitSum]);
+        }
+
+        for (auto digit : digitCountFreq) {
+            if (digit.second == maxDigitCount) {
+                maxDigitFreq++;
+            }
+        }
+        return maxDigitFreq;
+    }
+};
