fbd1956cf419ed55a4f0cb4569c5af099f8ba999
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-01-23 17:57:54 +0530

    Create 227_Basic_calculator_II.cpp

---
+++ Stack/227_Basic_calculator_II.cpp
@@ -0,0 +1,32 @@
+
+int calculate(string s) {//O(n) is is sc and tc
+    stack<int> myStack;
+    char sign = '+';
+    int res = 0, tmp = 0;
+    for (unsigned int i = 0; i < s.size(); i++) {
+        if (isdigit(s[i]))
+            tmp = 10*tmp + s[i]-'0';
+        if (!isdigit(s[i]) && !isspace(s[i]) || i == s.size()-1) {
+            if (sign == '-')
+                myStack.push(-tmp);
+            else if (sign == '+')
+                myStack.push(tmp);
+            else {
+                int num;
+                if (sign == '*' )
+                    num = myStack.top()*tmp;
+                else
+                    num = myStack.top()/tmp;
+                myStack.pop();
+                myStack.push(num);
+            } 
+            sign = s[i];
+            tmp = 0;
+        }
+    }
+    while (!myStack.empty()) {
+        res += myStack.top();
+        myStack.pop();
+    }
+    return res;
+}
