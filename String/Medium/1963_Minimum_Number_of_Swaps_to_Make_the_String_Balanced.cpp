254befbdf233128b42a64b2de1514affbbe7078a
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-02-01 22:18:26 +0530

    Create 1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp

---
+++ String/Medium/1963_Minimum_Number_of_Swaps_to_Make_the_String_Balanced.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public://O(N) and O(1) tc and sc
+    int minSwaps(string s) {
+    int balance = 0;
+    int maxImbalance = 0;
+
+    for (char c : s) {
+        if (c == '[') balance++;
+        else balance--;
+
+        if (balance < 0)
+            maxImbalance = max(maxImbalance, -balance);
+    }
+
+    return (maxImbalance + 1) / 2;
+}
+
+
+};
