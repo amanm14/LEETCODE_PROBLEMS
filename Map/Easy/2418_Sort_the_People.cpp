79405eb70a6e61252ec7022361a712cd9d2ce909
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-02-03 23:27:11 +0530

    Create 2418_Sort_the_People.cpp

---
+++ Map/Easy/2418_Sort_the_People.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+//map alsways keys in sorting order also 
+// Its TC is O(nlogn) and sc is O(n)
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        map<int, string>mp;
+        vector<string>v;
+        for(int i=0; i < size(names); i++)
+            mp[heights[i]] = names[i];
+        for (auto i = mp.rbegin(); i != mp.rend(); ++i) v.push_back(i->second);
+        return v;
+}
+};
