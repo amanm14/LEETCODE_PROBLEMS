2a0f3df0cb42482f2958074d0a6970c1c4586d78
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-03-28 16:41:31 +0530

    Create 606_Construct_String_from_Binary_Tree.cpp

---
+++ Binary_Tree/Easy/606_Construct_String_from_Binary_Tree.cpp
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public://O(N) is tc and sc
+    void solve(TreeNode*root,string&ans)
+    {
+        if(root==NULL) return ;
+        // char c=;
+        ans += to_string(root->val);
+        // if(root->left!=NULL||root->right!=NULL)ans.push_back('(');
+        // solve(root->left,ans);
+        // if(root->left==NULL&&root->right!=NULL) ans.push_back(')');
+
+        if(root->left!=NULL||root->right!=NULL)
+        {
+            ans.push_back('(');
+            solve(root->left,ans);
+            ans.push_back(')');
+        }
+        if(root->right!=NULL){
+            // ans.push_back(')');
+            ans.push_back('(');
+            solve(root->right,ans);
+            ans.push_back(')');
+        }
+        // ans.push_back(')');
+        // if(root->left==NULL&&root->right==NULL)ans.push_back(')');
+        return ;
+
+    }
+    string tree2str(TreeNode* root) {
+        string ans="";
+        solve(root,ans);
+        // ans.pop_back();
+        return ans;
+        
+    }
+};
