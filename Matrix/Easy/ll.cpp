44e914694f151205de5340095e2877482c909953
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-07-20 23:59:52 +0530

    Create ll.cpp

---
+++ Matrix/Easy/ll.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findDegrees(vector<vector<int>>& matrix) {
+        vector<int>ans(matrix.size());
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                ans[i]+=matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
