597dc431275b52bdaa4296532b5221fb88f2f557
Aman Mishra <89731641+amanm14@users.noreply.github.com>
2026-08-17 00:04:01 +0530

    Create 1248_Count_Number_of_Nice_Subarrays.cpp

---
+++ Array/Sliding_Window/1248_Count_Number_of_Nice_Subarrays.cpp
@@ -0,0 +1,48 @@
+// class Solution {
+// public:
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         int ans=0,temp=0,j=0;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(nums[i]%2!=0)
+//             {
+//                 temp++;
+//             }
+//             if(temp==k)
+//             {
+//                 ans+=i-j+1;
+//                 if(nums[j]%2!=0) temp--;
+//                 j++;
+//                 cout<<ans<<endl;
+//             }
+//         }
+//         return ans;
+//     }
+// };
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int ans = 0, oddCount = 0, left = 0;
+        int currentCount = 0; // Tracks consecutive even numbers at the start of the window
+
+        for (int right = 0; right < nums.size(); ++right) {
+            if (nums[right] % 2 != 0) {
+                oddCount++;
+                currentCount = 0; // Reset because we hit a new odd number
+            }
+
+            // Shrink the window while we have exactly k odd numbers
+            while (oddCount == k) {
+                currentCount++; // Every increment here represents a valid subarray ending at 'right'
+                if (nums[left] % 2 != 0) {
+                    oddCount--;
+                }
+                left++;
+            }
+            
+            // Add the number of valid subarrays found
+            ans += currentCount;
+        }
+        return ans;
+    }
+};
